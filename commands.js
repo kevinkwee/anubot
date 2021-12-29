04866c130da6da249b3a39230edc8a472207fb15
Michael Elian Kevin <michaelkevin47@gmail.com>
2021-12-29 23:30:10 +0700

    change logchat timestamp message format

--- commands.js
+++ commands.js
@@ -226,9 +226,9 @@ const logchat = new BotCommand(
                         const { t, s, op, d } = payload;
                         if (op == 0 && t == 'MESSAGE_CREATE') {
                             if (d.author.id == targetUid || `!` + d.author.id == targetUid) {
-                                let newMsgContent = lastContent + String.raw`\n> **[${(new Date(d.timestamp)).toLocaleString()}]**\n> *${(d.content).replace(regex, ` `).replace(/\\/g, ``)}*`;
+                                let newMsgContent = lastContent + String.raw`\n> **${d.timestamp}**\n> *${(d.content).replace(regex, ` `).replace(/\\/g, ``)}*`;
                                 if (newMsgContent.length >= 2000) {
-                                    newMsgContent = String.raw`**Catetan chat <@${targetUid}>**\n*klo bot ini spam salahin <@${msgData.author.id}>*` + String.raw`\n> **[${(new Date(d.timestamp)).toLocaleString()}]**\n> *${(d.content).replace(regex, ` `).replace(/\\/g, ``)}*`;
+                                    newMsgContent = String.raw`**Catetan chat <@${targetUid}>**\n*klo bot ini spam salahin <@${msgData.author.id}>*` + String.raw`\n> **${d.timestamp}**\n> *${(d.content).replace(regex, ` `).replace(/\\/g, ``)}*`;
                                 }
                                 utils().sendMessage(guildId, channelId, newMsgContent).then((newResponse) => {
                                     lastContent = newResponse.data.content.replace(regex, `\\n`);
