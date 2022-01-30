1dd7d0122d758c30496fa49f4e8e6fea91033d26
Michael Elian Kevin <michaelkevin47@gmail.com>
2022-01-30 20:31:15 +0700

    debugging voice; fixing resume connection;

--- app.js
+++ app.js
@@ -84,14 +84,12 @@ function connect(isResuming = false) {
         }
     });
 
-    ws.on('close', (_, code, reason) => {
+    ws.on('close', () => {
         clearInterval(interval);
         heartbeatAck = false;
         zombieHeartbeatCount = 0;
         console.log("[" + utils().getCurrentTimeStr() + "] " + "Websocket closed.");
         console.log("[" + utils().getCurrentTimeStr() + "] " + "Bye bye...");
-        console.log(`Closed code: ${code}`);
-        console.log(`Reason: ${reason.toString()}`);
     });
 
     ws.on('message', (data) => {
